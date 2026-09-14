7b7f2a3dc90206364e7e30280d9d7909566ed56c
Gomotsegang1 <119848229+Gomotsegang1@users.noreply.github.com>
2026-09-14 14:22:05 +0200

    Update 01_setup.sql
    Add customers table

--- sql/01_setup.sql
+++ sql/01_setup.sql
@@ -1,12 +1,18 @@
 -- Kasi Mart Data Engineering Project 1
--- Snowflake setup
+-- Snowflake setup and table creation
 
-CREATE DATABASE DE_PROJECT1;
+-- Database and schema were created in Snowflake
+-- using Catalog -> Databases -> Create
+-- and Schema -> Create.
 
 USE DATABASE DE_PROJECT1;
-
-CREATE SCHEMA KASI_MART;
-
 USE SCHEMA KASI_MART;
 
-SELECT CURRENT_DATABASE(), CURRENT_SCHEMA();
+-- Customers dimension table
+CREATE TABLE CUSTOMERS (
+    customer_id VARCHAR(20),
+    customer_name VARCHAR(100),
+    email VARCHAR(255),
+    province VARCHAR(50),
+    signup_date DATE
+);
